1863421523059dab24842e57516511263f1f2c43
development-webtitans <development@webtitans.net>
2023-01-15 20:10:49 +0500

    firt push

--- src/Components/Login-Signup/LoginSignup.jsx
+++ src/Components/Login-Signup/LoginSignup.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-export default function LoginSignup() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function LoginForm() {
+  const classes = useStyles();
+
   return (
-    <div>
-      kgkjhgkjhgkjhgkhkyflgtytliutlugkghjhgkhglghkhgkhjgkjhgjhmgl,hjbmnmh,gliu,khb,mnbmg,kmbnnfsadfsadfasdfasdfsadfsdfsadfasdfasfg,fg,bkh,kgjbvhghvgbvhj,gmhvjgmhmvgjmjgmjhgmmgjhghgjmjmjjmgjmhgmhgmhgghghmghhfk,hgfkjgjhgkjgjhjfkj,gf
-    </div>
+    <form className={classes.root} noValidate autoComplete="off">
+      <div>
+        <TextField id="standard-basic" label="Username" variant="outlined" />
+        <TextField
+          id="standard-basic"
+          label="Password"
+          type="password"
+          variant="outlined"
+        />
+      </div>
+      <Button variant="contained" className={classes.button}>
+        Login
+      </Button>
+    </form>
   );
 }
